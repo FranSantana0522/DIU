a22464fa24d06d3373271dd48aca7b7db6d179c6
FranSantana0522 <franciscojaviermontes2014@gmail.com>
2024-03-07 22:05:10 +0100

    Front terminado

--- React/agendafront/src/http-common.js
+++ React/agendafront/src/http-common.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
+  //Agenda-env.eba-r52h5aj9.us-east-1.elasticbeanstalk.com
+  //192.168.100.11:8099
   baseURL: "http://Agenda-env.eba-r52h5aj9.us-east-1.elasticbeanstalk.com/api/v1",
   headers: {
     "Content-type": "application/json"
